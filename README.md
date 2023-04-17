382474c8c4b7c95e73add9c562abeefe0b5e7213
leaker <admin@leelib.com>
2023-04-17 19:52:16 +0700

    add long_description

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# wozoxutils-py
+# wozoxutils [![PyPI](https://img.shields.io/pypi/v/wozoxutils.svg)](https://pypi.org/project/wozoxutils/) [![PyPI - Format](https://img.shields.io/pypi/format/wozoxutils.svg)](https://pypi.org/project/wozoxutils/) [![PyPI - Python Version](https://img.shields.io/pypi/pyversions/wozoxutils.svg)](https://pypi.org/project/wozoxutils/)
 general-purpose library for Python that contains various commonly used functional modules.
+
+## Installation
+
+```sh
+pip install wozoxutils
+```
+
--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+import os
 from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name="wozoxutils",
-    version="0.2",
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[],  # 添加依赖包
     author="illiten",
     author_email="admin@leelib.com",
     description="general-purpose library for Python that contains various commonly used functional modules",
     url="https://github.com/wozox/wozoxutils-py",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='wozox utils',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
